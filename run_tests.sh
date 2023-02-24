bf3cbfdd06437bc9bdefdf5c250ffec01b80e60e
Nico Coetzee <nicc777@gmail.com>
2023-02-24 08:29:40 +0100

    test: adding initial tests

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -frR reports
+mkdir reports
+
+
+echo ; echo ; echo "########################################################################################################################"
+coverage run -a tests/test_plugins.py
+
+echo ; echo ; echo "########################################################################################################################"
+coverage report --omit="tests/test*" -m
+coverage html -d reports --omit="tests/test*"
+coverage report --format=markdown --omit="tests/test*" > doc/coverage/README.md
+echo "Report available in file://$PWD/reports/index.html"
+
