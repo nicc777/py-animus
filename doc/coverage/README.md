ea74512874d63c6e5dd2b54ab2fd6ba7019b3054
Nico Coetzee <nicc777@gmail.com>
2023-03-05 10:51:31 +0100

    feat: [#7] Adding discovered versions for class instances

--- doc/coverage/README.md
+++ doc/coverage/README.md
@@ -1,5 +1,5 @@
 | Name                                   |    Stmts |     Miss |   Cover |
 |--------------------------------------- | -------: | -------: | ------: |
 | src/py\_animus/\_\_init\_\_.py         |       34 |        0 |    100% |
-| src/py\_animus/manifest\_management.py |      174 |        1 |     99% |
-|                              **TOTAL** |  **208** |    **1** | **99%** |
+| src/py\_animus/manifest\_management.py |      206 |        9 |     96% |
+|                              **TOTAL** |  **240** |    **9** | **96%** |
--- src/py_animus/manifest_management.py
+++ src/py_animus/manifest_management.py
@@ -599,8 +599,10 @@ class ManifestManager:
         manifest_instance = self.get_manifest_instance_by_name(name=name)
         manifest_instance.apply_manifest(manifest_lookup_function=self.get_manifest_instance_by_name, variable_cache=self.variable_cache)
 
-    def delete_manifest(self, name: str):
-        manifest_instance = self.get_manifest_instance_by_name(name=name)
+    def delete_manifest(self, name: str, version: str):
+        if version is None:
+            raise Exception('Version is required')
+        manifest_instance = self.get_manifest_instance_by_name(name=name, version=version)
         manifest_instance.delete_manifest(manifest_lookup_function=self.get_manifest_instance_by_name, variable_cache=self.variable_cache)
 
     def get_manifest_class_by_kind(self, kind: str, version: str=None):
