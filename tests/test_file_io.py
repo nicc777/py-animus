2ab6cff34c267555590721e531f7905d49b7cc58
Nico Coetzee <nicc777@gmail.com>
2023-05-05 08:09:12 +0200

    test: #71 - Preparing test data

--- tests/test_file_io.py
+++ tests/test_file_io.py
@@ -48,6 +48,97 @@ class TestFileIoCreateDirs(unittest.TestCase):    # pragma: no cover
         self.assertFalse(os.path.exists(tmp_dir))
 
 
+class TestFileIoRemainingFunctions(unittest.TestCase):    # pragma: no cover
+
+    def setUp(self):
+        print('-'*80)
+        self.tmp_dir = create_temp_directory()
+        self.dir_setup_data = (
+            {
+                'dir': '{}{}dir1'.format(self.tmp_dir, os.sep),
+                'files': [
+                    {
+                        'file1.txt': 'content {}'.format(generate_random_string(length=8)) # Total length = 8+8 = 16
+                    },
+                    {
+                        'file2.txt': 'content {}'.format(generate_random_string(length=16)) # Total length = 8+16 = 24
+                    },
+                ],
+            },
+            {
+                'dir': '{}{}dir2'.format(self.tmp_dir, os.sep),
+                'files': [
+                    {
+                        'file3.txt': 'content {}'.format(generate_random_string(length=16)) 
+                    },
+                    {
+                        'file4.txt': 'content {}'.format(generate_random_string(length=32)) 
+                    },
+                ],
+            },
+            {
+                'dir': '{}{}dir1{}subdir1'.format(self.tmp_dir, os.sep, os.sep),
+                'files': [
+                    {
+                        'file5.txt': 'content {}'.format(generate_random_string(length=12)) 
+                    },
+                    {
+                        'file6.txt': 'content {}'.format(generate_random_string(length=22)) 
+                    },
+                    {
+                        'file7.txt': 'content {}'.format(generate_random_string(length=32)) 
+                    },
+                    {
+                        'file8.txt': 'content {}'.format(generate_random_string(length=42)) 
+                    },
+                ],
+            },
+            {
+                'dir': '{}{}dir1{}subdir2'.format(self.tmp_dir, os.sep, os.sep),
+                'files': [
+                    {
+                        'file9.txt': 'content {}'.format(generate_random_string(length=52)) 
+                    },
+                    {
+                        'file10.txt': 'content {}'.format(generate_random_string(length=62)) 
+                    },
+                ],
+            },
+            {
+                'dir': '{}{}dir2{}subdir3'.format(self.tmp_dir, os.sep, os.sep),
+                'files': [],
+            },
+            {
+                'dir': '{}{}dir2{}subdir3{}subdir4'.format(self.tmp_dir, os.sep, os.sep, os.sep),
+                'files': [
+                    {
+                        'file11.txt': 'content {}'.format(generate_random_string(length=72)) 
+                    },
+                    {
+                        'file12.txt': 'content {}'.format(generate_random_string(length=82)) 
+                    },
+                    {
+                        'file13.txt': 'content {}'.format(generate_random_string(length=92)) 
+                    },
+                ],
+            },
+        )
+        print('PREPARING DIRECTORIES AND FILES')
+        for dir in self.dir_setup_data:
+            for dir_name, file_data in dir.items():
+                create_directory(path=dir_name)
+                print('* Created directory "{}"'.format(dir_name))
+        
+    def tearDown(self):
+        print('DELETING DIRECTORIES')
+        for dir in self.dir_setup_data:
+            for dir_name, file_data in dir.items():
+                delete_directory(dir=dir_name)
+                print('* Deleted directory "{}"'.format(dir_name))
+
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
