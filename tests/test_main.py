9e912a6033ce43ecfc9d63072a2355e6b45947c4
Nico Coetzee <nicc777@gmail.com>
2023-08-31 06:19:40 +0200

    test: #83 - Added local http serer for testing remote files

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,8 @@ import sys
 import os
 import json
 import tempfile
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import _thread as thread
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 print('sys.path={}'.format(sys.path))
 
@@ -18,7 +20,20 @@ import unittest
 
 from py_animus.animus import run_main
 
-class TestClassMain(unittest.TestCase):    # pragma: no cover
+
+host_name_01 = "localhost"
+server_port_01 = 7999
+
+
+class TestHttpServerBasic(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("test", "utf-8"))
+
+
+class TestClassMainBasic01(unittest.TestCase):    # pragma: no cover
 
     def setUp(self):
         print('-'*80)
@@ -31,9 +46,25 @@ class TestClassMain(unittest.TestCase):    # pragma: no cover
         os.unlink(self.manifest_file)
         os.rmdir(self.tmp_dir.name)
 
+    @classmethod
+    def setUpClass(cls):
+        cls.web_server_01 = HTTPServer((host_name_01, server_port_01), TestHttpServerBasic)
+        print("Server started http://%s:%s" % (host_name_01, server_port_01))
+        def start_server():
+            cls.web_server_01.serve_forever()
+        thread.start_new_thread(start_server, ())
 
-    def test_basic_init(self):
+    @classmethod
+    def tearDownClass(cls):
+        cls.web_server_01.server_close()
+
+    def test_basic_init_from_local_file(self):
         result = run_main(cli_parameter_overrides=['animus.py', 'apply', self.manifest_file, 'project', 'test-scope'])
+        self.assertIsNotNone(result)
+        self.assertTrue(result)
+
+    def test_basic_init_from_http_server(self):
+        result = run_main(cli_parameter_overrides=['animus.py', 'apply', 'http://{}:{}/'.format(host_name_01, server_port_01), 'project', 'test-scope'])
         self.assertIsNotNone(result)
         self.assertTrue(result)
 
